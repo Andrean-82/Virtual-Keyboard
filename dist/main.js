edbe03f03c2b585848649f6096c11db0a59377e7
Andrean-82 <85643505+Andrean-82@users.noreply.github.com>
2022-05-12 21:48:48 +0500

    feat: add dist

---
+++ dist/main.js
@@ -0,0 +1,776 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/Keyboard.js":
+/*!*************************!*\
+  !*** ./src/Keyboard.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _impexp_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./impexp.js */ "./src/impexp.js");
+/* eslint-disable import/extensions */
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
+  Lang: _impexp_js__WEBPACK_IMPORTED_MODULE_0__["default"]
+});
+
+/***/ }),
+
+/***/ "./src/impexp.js":
+/*!***********************!*\
+  !*** ./src/impexp.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _language_ruskey__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./language/ruskey */ "./src/language/ruskey.js");
+/* harmony import */ var _language_engkey__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./language/engkey */ "./src/language/engkey.js");
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
+  ruskey: _language_ruskey__WEBPACK_IMPORTED_MODULE_0__["default"],
+  engkey: _language_engkey__WEBPACK_IMPORTED_MODULE_1__["default"]
+});
+
+/***/ }),
+
+/***/ "./src/language/engkey.js":
+/*!********************************!*\
+  !*** ./src/language/engkey.js ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ([{
+  small: '`',
+  shift: '~',
+  code: 'Backquote'
+}, {
+  small: '1',
+  shift: '!',
+  code: 'Digit1'
+}, {
+  small: '2',
+  shift: '@',
+  code: 'Digit2'
+}, {
+  small: '3',
+  shift: '#',
+  code: 'Digit3'
+}, {
+  small: '4',
+  shift: '$',
+  code: 'Digit4'
+}, {
+  small: '5',
+  shift: '%',
+  code: 'Digit5'
+}, {
+  small: '6',
+  shift: '^',
+  code: 'Digit6'
+}, {
+  small: '7',
+  shift: '&',
+  code: 'Digit7'
+}, {
+  small: '8',
+  shift: '*',
+  code: 'Digit8'
+}, {
+  small: '9',
+  shift: '(',
+  code: 'Digit9'
+}, {
+  small: '0',
+  shift: ')',
+  code: 'Digit0'
+}, {
+  small: '-',
+  shift: '_',
+  code: 'Minus'
+}, {
+  small: '=',
+  shift: '+',
+  code: 'Equal'
+}, {
+  small: 'Backspace',
+  shift: '',
+  code: 'Backspace'
+}, {
+  small: 'Tab',
+  shift: 'null',
+  code: 'Tab'
+}, {
+  small: 'q',
+  shift: 'Q',
+  code: 'KeyQ'
+}, {
+  small: 'w',
+  shift: 'W',
+  code: 'KeyW'
+}, {
+  small: 'e',
+  shift: 'E',
+  code: 'KeyE'
+}, {
+  small: 'r',
+  shift: 'R',
+  code: 'KeyR'
+}, {
+  small: 't',
+  shift: 'T',
+  code: 'KeyT'
+}, {
+  small: 'y',
+  shift: 'Y',
+  code: 'KeyY'
+}, {
+  small: 'u',
+  shift: 'U',
+  code: 'KeyU'
+}, {
+  small: 'i',
+  shift: 'I',
+  code: 'KeyI'
+}, {
+  small: 'o',
+  shift: 'O',
+  code: 'KeyO'
+}, {
+  small: 'p',
+  shift: 'P',
+  code: 'KeyP'
+}, {
+  small: '[',
+  shift: '{',
+  code: 'BracketLeft'
+}, {
+  small: ']',
+  shift: '}',
+  code: 'BracketRight'
+}, {
+  small: '\\',
+  shift: '|',
+  code: 'Backslash'
+}, {
+  small: 'DEL',
+  shift: 'null',
+  code: 'Delete'
+}, {
+  small: 'CapsLock',
+  shift: 'null',
+  code: 'CapsLock'
+}, {
+  small: 'a',
+  shift: 'A',
+  code: 'KeyA'
+}, {
+  small: 's',
+  shift: 'S',
+  code: 'KeyS'
+}, {
+  small: 'd',
+  shift: 'D',
+  code: 'KeyD'
+}, {
+  small: 'f',
+  shift: 'F',
+  code: 'KeyF'
+}, {
+  small: 'g',
+  shift: 'G',
+  code: 'KeyG'
+}, {
+  small: 'h',
+  shift: 'H',
+  code: 'KeyH'
+}, {
+  small: 'j',
+  shift: 'J',
+  code: 'KeyJ'
+}, {
+  small: 'k',
+  shift: 'K',
+  code: 'KeyK'
+}, {
+  small: 'l',
+  shift: 'L',
+  code: 'KeyL'
+}, {
+  small: ';',
+  shift: ':',
+  code: 'Semicolon'
+}, {
+  small: "'",
+  shift: '"',
+  code: 'Quote'
+}, {
+  small: 'ENTER',
+  shift: 'null',
+  code: 'Enter'
+}, {
+  small: 'Shift',
+  shift: 'null',
+  code: 'ShiftLeft'
+}, {
+  small: '\\',
+  shift: 'null',
+  code: 'IntlSlash'
+}, {
+  small: 'z',
+  shift: 'Z',
+  code: 'KeyZ'
+}, {
+  small: 'x',
+  shift: 'X',
+  code: 'KeyX'
+}, {
+  small: 'c',
+  shift: 'C',
+  code: 'KeyC'
+}, {
+  small: 'v',
+  shift: 'V',
+  code: 'KeyV'
+}, {
+  small: 'b',
+  shift: 'B',
+  code: 'KeyB'
+}, {
+  small: 'n',
+  shift: 'N',
+  code: 'KeyN'
+}, {
+  small: 'm',
+  shift: 'M',
+  code: 'KeyM'
+}, {
+  small: ',',
+  shift: '<',
+  code: 'Comma'
+}, {
+  small: '.',
+  shift: '>',
+  code: 'Period'
+}, {
+  small: '/',
+  shift: '?',
+  code: 'Slash'
+}, {
+  small: 'Shift',
+  shift: 'null',
+  code: 'ShiftRight'
+}, {
+  small: 'Ctrl',
+  shift: 'null',
+  code: 'ControlLeft'
+}, {
+  small: 'Win',
+  shift: 'null',
+  code: 'OSLeft'
+}, {
+  small: 'Alt',
+  shift: 'null',
+  code: 'AltLeft'
+}, {
+  small: ' ',
+  shift: 'null',
+  code: 'Space'
+}, {
+  small: 'Alt',
+  shift: 'null',
+  code: 'AltRight'
+}, {
+  small: 'Ctrl',
+  shift: 'null',
+  code: 'ControlRight'
+}, {
+  small: 'Left',
+  shift: 'null',
+  code: 'ArrowLeft'
+}, {
+  small: 'Up',
+  shift: 'null',
+  code: 'ArrowUp'
+}, {
+  small: 'Down',
+  shift: 'null',
+  code: 'ArrowDown'
+}, {
+  small: 'Right',
+  shift: 'null',
+  code: 'ArrowRight'
+}]);
+
+/***/ }),
+
+/***/ "./src/language/ruskey.js":
+/*!********************************!*\
+  !*** ./src/language/ruskey.js ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ([{
+  small: 'ё',
+  shift: 'Ё',
+  code: 'Backquote'
+}, {
+  small: '1',
+  shift: '!',
+  code: 'Digit1'
+}, {
+  small: '2',
+  shift: '"',
+  code: 'Digit2'
+}, {
+  small: '3',
+  shift: '№',
+  code: 'Digit3'
+}, {
+  small: '4',
+  shift: ';',
+  code: 'Digit4'
+}, {
+  small: '5',
+  shift: '%',
+  code: 'Digit5'
+}, {
+  small: '6',
+  shift: ':',
+  code: 'Digit6'
+}, {
+  small: '7',
+  shift: '?',
+  code: 'Digit7'
+}, {
+  small: '8',
+  shift: '*',
+  code: 'Digit8'
+}, {
+  small: '9',
+  shift: '(',
+  code: 'Digit9'
+}, {
+  small: '0',
+  shift: ')',
+  code: 'Digit0'
+}, {
+  small: '-',
+  shift: '_',
+  code: 'Minus'
+}, {
+  small: '=',
+  shift: '+',
+  code: 'Equal'
+}, {
+  small: 'Backspace',
+  shift: '',
+  code: 'Backspace'
+}, {
+  small: 'Tab',
+  shift: 'null',
+  code: 'Tab'
+}, {
+  small: 'й',
+  shift: 'Й',
+  code: 'KeyQ'
+}, {
+  small: 'ц',
+  shift: 'Ц',
+  code: 'KeyW'
+}, {
+  small: 'у',
+  shift: 'У',
+  code: 'KeyE'
+}, {
+  small: 'к',
+  shift: 'К',
+  code: 'KeyR'
+}, {
+  small: 'е',
+  shift: 'Е',
+  code: 'KeyT'
+}, {
+  small: 'н',
+  shift: 'Н',
+  code: 'KeyY'
+}, {
+  small: 'г',
+  shift: 'Г',
+  code: 'KeyU'
+}, {
+  small: 'ш',
+  shift: 'Ш',
+  code: 'KeyI'
+}, {
+  small: 'щ',
+  shift: 'Щ',
+  code: 'KeyO'
+}, {
+  small: 'з',
+  shift: 'З',
+  code: 'KeyP'
+}, {
+  small: 'х',
+  shift: 'Х',
+  code: 'BracketLeft'
+}, {
+  small: 'ъ',
+  shift: 'Ъ',
+  code: 'BracketRight'
+}, {
+  small: '\\',
+  shift: '/',
+  code: 'Backslash'
+}, {
+  small: 'DEL',
+  shift: 'null',
+  code: 'Delete'
+}, {
+  small: 'CapsLock',
+  shift: 'null',
+  code: 'CapsLock'
+}, {
+  small: 'ф',
+  shift: 'Ф',
+  code: 'KeyA'
+}, {
+  small: 'ы',
+  shift: 'Ы',
+  code: 'KeyS'
+}, {
+  small: 'в',
+  shift: 'В',
+  code: 'KeyD'
+}, {
+  small: 'а',
+  shift: 'А',
+  code: 'KeyF'
+}, {
+  small: 'п',
+  shift: 'П',
+  code: 'KeyG'
+}, {
+  small: 'р',
+  shift: 'Р',
+  code: 'KeyH'
+}, {
+  small: 'о',
+  shift: 'О',
+  code: 'KeyJ'
+}, {
+  small: 'л',
+  shift: 'Л',
+  code: 'KeyK'
+}, {
+  small: 'д',
+  shift: 'Д',
+  code: 'KeyL'
+}, {
+  small: 'ж',
+  shift: 'Ж',
+  code: 'Semicolon'
+}, {
+  small: 'э',
+  shift: 'Э',
+  code: 'Quote'
+}, {
+  small: '',
+  shift: '',
+  code: ''
+}, {
+  small: 'ENTER',
+  shift: 'null',
+  code: 'Enter'
+}, {
+  small: 'Shift',
+  shift: 'null',
+  code: 'ShiftLeft'
+}, {
+  small: '\\',
+  shift: 'null',
+  code: 'IntlSlash'
+}, {
+  small: 'я',
+  shift: 'Я',
+  code: 'KeyZ'
+}, {
+  small: 'ч',
+  shift: 'Ч',
+  code: 'KeyX'
+}, {
+  small: 'с',
+  shift: 'С',
+  code: 'KeyC'
+}, {
+  small: 'м',
+  shift: 'М',
+  code: 'KeyV'
+}, {
+  small: 'и',
+  shift: 'И',
+  code: 'KeyB'
+}, {
+  small: 'т',
+  shift: 'Т',
+  code: 'KeyN'
+}, {
+  small: 'ь',
+  shift: 'Ь',
+  code: 'KeyM'
+}, {
+  small: 'б',
+  shift: 'Б',
+  code: 'Comma'
+}, {
+  small: 'ю',
+  shift: 'Ю',
+  code: 'Period'
+}, {
+  small: '.',
+  shift: ',',
+  code: 'Slash'
+}, {
+  small: 'Shift',
+  shift: 'null',
+  code: 'ShiftRight'
+}, {
+  small: 'Ctrl',
+  shift: 'hull',
+  code: 'ControlLeft'
+}, {
+  small: 'Win',
+  shift: 'null',
+  code: 'OSLeft'
+}, {
+  small: 'Alt',
+  shift: 'null',
+  code: 'AltLeft'
+}, {
+  small: '',
+  shift: 'null',
+  code: 'Space'
+}, {
+  small: 'Alt',
+  shift: 'null',
+  code: 'AltRight'
+}, {
+  small: 'Ctrl',
+  shift: 'null',
+  code: 'ControlRight'
+}, {
+  small: 'Left',
+  shift: 'null',
+  code: 'ArrowLeft'
+}, {
+  small: 'Up',
+  shift: 'null',
+  code: 'ArrowUp'
+}, {
+  small: 'Down',
+  shift: 'null',
+  code: 'ArrowDown'
+}, {
+  small: 'Right',
+  shift: 'null',
+  code: 'ArrowRight'
+}]);
+
+/***/ }),
+
+/***/ "./src/style.css":
+/*!***********************!*\
+  !*** ./src/style.css ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!***********************!*\
+  !*** ./src/script.js ***!
+  \***********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _Keyboard__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Keyboard */ "./src/Keyboard.js");
+/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./style.css */ "./src/style.css");
+/* eslint-disable no-undef */
+
+/* eslint-disable no-use-before-define */
+
+
+const Keys = [['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'], ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete'], ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'], ['ShiftLeft', 'IntlSlash', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight'], ['ControlLeft', 'OSLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight']];
+let text;
+let N = window.localStorage.getItem('Keyboard') || 'en';
+
+if (N === 'en') {
+  text = _Keyboard__WEBPACK_IMPORTED_MODULE_0__["default"].Lang.engkey;
+} else {
+  text = _Keyboard__WEBPACK_IMPORTED_MODULE_0__["default"].Lang.ruskey;
+}
+
+let keyOne;
+let Id;
+const array = [];
+const body = document.querySelector('body');
+const divcreat = document.createElement('div');
+divcreat.classList.add('area');
+divcreat.textContent = 'Реализованона Windows 10, держать Shif + нажать Alt переключение языка :-))';
+body.appendChild(divcreat);
+const win = document.createElement('div');
+win.classList.add('window');
+divcreat.append(win);
+let wrapperKeyboard = document.createElement('div');
+wrapperKeyboard.classList.add('wrapper-keyboard');
+divcreat.append(wrapperKeyboard);
+document.addEventListener('keydown', event => {
+  const isShift = 'Shift';
+
+  if (isShift && event.key === 'Alt') {
+    divcreat.removeChild(wrapperKeyboard);
+
+    if (N === 'en') {
+      wrapperKeyboard = document.createElement('div');
+      wrapperKeyboard.classList.add('wrapper-keyboard');
+      divcreat.append(wrapperKeyboard);
+      text = _Keyboard__WEBPACK_IMPORTED_MODULE_0__["default"].Lang.ruskey;
+      localStorage.setItem('Keyboard', 'ru');
+      N = 'ru';
+      overkill();
+    } else {
+      wrapperKeyboard = document.createElement('div');
+      wrapperKeyboard.classList.add('wrapper-keyboard');
+      divcreat.append(wrapperKeyboard);
+      text = _Keyboard__WEBPACK_IMPORTED_MODULE_0__["default"].Lang.engkey;
+      localStorage.setItem('Keyboard', 'en');
+      N = 'en';
+      overkill();
+    }
+  }
+});
+
+function overkill() {
+  for (let i = 0; i < Keys.length; i += 1) {
+    for (let j = 0; j < Keys[i].length; j += 1) {
+      keyOne = document.createElement('div');
+      keyOne.id = Keys[i][j];
+      keyOne.classList.add('button');
+      keyOne.addEventListener('click', createdKey, false);
+      Id = text.find(val => val.code === keyOne.id).small;
+      keyOne.textContent = Id;
+      wrapperKeyboard.append(keyOne);
+    }
+  }
+}
+
+overkill();
+
+function createdKey(e) {
+  e.preventDefault();
+  const eventKeq = e.target;
+  const button = this.id;
+
+  if (eventKeq.id === 'CapsLock') {
+    Id = text.find(val => val.code === button).shift;
+    array.push(win.textContent = Id);
+    return text.shift;
+  }
+
+  if (eventKeq.id !== 'CapsLock') {
+    eventKeq.classList.toggle('not-shadow');
+    const button = this.id;
+    Id = text.find(val => val.code === button).small;
+    array.push(win.textContent = Id);
+
+    if (eventKeq.id === 'Backspace') {
+      array.pop();
+      array.pop();
+    }
+
+    win.textContent = array.join('');
+    const elem = document.querySelector('.not-shadow');
+    setTimeout(() => elem.classList.remove('not-shadow'), 500);
+  }
+}
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
